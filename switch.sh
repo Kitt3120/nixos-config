9162c3dfb8ad3939720b9f94051b2bf9d5dfa079
Torben Schweren <torben@schweren.dev>
2024-03-16 20:41:43 +0100

    Switch script

---
+++ switch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo mv /etc/nixos/hardware-configuration.nix /tmp/
+sudo rm -rf /etc/nixos/*
+sudo cp -r ./* /etc/nixos
+sudo rm -rf /etc/nixos/switch.sh /etc/nixos/.gitignore /etc/nixos/.git
+sudo mv /tmp/hardware-configuration.nix /etc/nixos/
+sudo chown root:root -R /etc/nixos/
+sudo nixos-rebuild switch
