d56082eef22822e8851afb7e79f6fe10f4153456
Torben Schweren <torben@schweren.dev>
2024-03-17 20:01:18 +0100

    Improve Steam installation

--- programs/per-device/steam.nix
+++ programs/per-device/steam.nix
@@ -1,5 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ steam ];
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    extest.enable = true;
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  hardware.steam-hardware.enable = true;
 }
