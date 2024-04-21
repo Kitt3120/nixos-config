a64f90b64d060240ba105a9d8b154d5eb8355ae5
Torben Schweren <torben@schweren.dev>
2024-04-21 20:29:22 +0200

    Add ssh blocks

--- per-device/hydra/users/torben/ssh.nix
+++ per-device/hydra/users/torben/ssh.nix
@@ -1,5 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-    users.users.torben.openssh.authorizedKeys.keys = config.credentials.authorizedKeys;
+  users.users.torben.openssh.authorizedKeys.keys = config.credentials.authorizedKeys;
+
+  home-manager.users.torben.home.file.".ssh/config".text = lib.strings.concatStrings [
+    config.ssh.blocks.mrmeeseeks
+    config.ssh.blocks.schweren-dev
+  ];
 }
