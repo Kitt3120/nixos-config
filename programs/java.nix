c2c662d5dd8bba69d81e4d05d2c19b945080f414
Torben Schweren <torben@schweren.dev>
2024-05-03 00:18:54 +0200

    Update Java

--- programs/java.nix
+++ programs/java.nix
@@ -3,13 +3,22 @@
 {
   programs.java = {
     enable = true;
-    package = pkgs.openjdk21; # Set to same as first Java below
+    package = pkgs.jdk; # Set to same as first Java below
   };
   
   environment.systemPackages = with pkgs; [ 
     # First option is default Java
-    openjdk21
-    openjdk17
-    openjdk8
+    jdk
+    jdk21
+    jdk17
+    jdk8
    ];
+
+   environment.variables = {
+      JAVA_HOME = "${pkgs.jdk}";
+      JAVA_LATEST_HOME = "${pkgs.jdk}";
+      JAVA_21_HOME = "${pkgs.jdk21}";
+      JAVA_17_HOME = "${pkgs.jdk17}";
+      JAVA_8_HOME = "${pkgs.jdk8}";
+   };
 }
