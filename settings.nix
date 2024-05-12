94ca4c1c0be60d3f2fefe8215c30d5642976ea4b
Torben Schweren <torben@schweren.dev>
2024-05-12 10:53:09 +0200

    Update and rename credentials.nix to settings.nix

---
+++ settings.nix
@@ -0,0 +1,11 @@
+# Untrack this file from git by running
+# git update-index --assume-unchanged settings.nix
+# Then overwrite settings defined in settings-defaults.nix
+
+{ config, pkgs, lib, ... }:
+
+{
+  config = {
+    
+  };
+}
