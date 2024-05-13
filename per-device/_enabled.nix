9668900ce960d9f8c5c2b7c7c8533e44d66570d5
Torben Schweren <torben@schweren.dev>
2024-05-13 16:50:31 +0200

    Improve setup.sh
    Now automatically untracking settings.nix and per-device/_enabled.nix

--- per-device/_enabled.nix
+++ per-device/_enabled.nix
@@ -1,3 +1,8 @@
+# Untrack this file from git by running
+# git update-index --assume-unchanged per-device/_enabled.nix
+# Or by running setup.sh
+# Then set current device profile below
+
 { config, pkgs, ... }:
 
 {
--- settings.nix
+++ settings.nix
@@ -1,5 +1,6 @@
 # Untrack this file from git by running
 # git update-index --assume-unchanged settings.nix
+# Or by running setup.sh
 # Then overwrite settings defined in settings-defaults.nix
 
 { config, pkgs, lib, ... }:
