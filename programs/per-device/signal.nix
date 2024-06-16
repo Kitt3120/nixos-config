3f4ad7cea0641cec53b17be1b74820d795da1884
Torben Schweren <torben@schweren.dev>
2024-06-16 19:30:11 +0200

    add: note for signal-desktop

--- programs/per-device/signal.nix
+++ programs/per-device/signal.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 
 {
+  # If signal-desktop starts minimized, edit ~/.config/Signal/ephemeral.json and set "maximized" to true
   environment.systemPackages = with pkgs; [ signal-desktop ];
 }
