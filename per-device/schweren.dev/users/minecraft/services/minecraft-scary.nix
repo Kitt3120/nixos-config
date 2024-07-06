23ac8e9afafcf7deaa1b23e86ca169d6fdeac6dd
Torben Schweren <torben@schweren.dev>
2024-07-06 16:05:32 +0200

    fix: use jdk21

--- per-device/schweren.dev/users/minecraft/services/minecraft-scary.nix
+++ per-device/schweren.dev/users/minecraft/services/minecraft-scary.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  scary = config.mkMinecraftFabricServer "scary" "/home/minecraft/scary" "fabric" pkgs.jdk17 "10G" false; # TODO: Put Minecraft servers into settings
+  scary = config.mkMinecraftFabricServer "scary" "/home/minecraft/scary" "fabric" pkgs.jdk21 "10G" false; # TODO: Put Minecraft servers into settings
 in
 {
   home-manager.users.minecraft = {
