1dbce31706bdefbd43a8d01b323b6d66a8f24553
Torben Schweren <torben@schweren.dev>
2024-08-02 15:30:03 +0200

    add: cargo to fish path

--- programs/fish.nix
+++ programs/fish.nix
@@ -5,7 +5,9 @@
   users.defaultUserShell = pkgs.fish;
 
   home-manager.users = config.mapAllUsersToSet (user: {
-    "${user}".xdg.configFile."fish/conf.d/disable_greeting.fish".text = "set fish_greeting";
+    "${user}".xdg.configFile = {
+      "fish/conf.d/disable_greeting.fish".text = "set fish_greeting";
+      "fish/conf.d/cargo.fish".text = "fish_add_path $HOME/.cargo/bin";
+    };
   });
-
 }
