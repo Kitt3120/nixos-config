dfab092fb27a3ebf918123aaba21ad7406b0c642
Torben Schweren <torben@schweren.dev>
2024-08-06 22:05:47 +0200

    add: new stylix wallpaper

--- desktop/per-device/stylix.nix
+++ desktop/per-device/stylix.nix
@@ -15,10 +15,12 @@ in {
     polarity = "dark";
 
     image = pkgs.fetchurl {
-      url = "https://www.pixelstalk.net/wp-content/uploads/2016/10/Anime-Landscape-Wallpaper-HD.jpg";
-      sha256 = "12f275862b66e99973c933b4471574e8d1bb2997b6752c47be510e084c427c9a";
+      #url = "https://www.pixelstalk.net/wp-content/uploads/2016/10/Anime-Landscape-Wallpaper-HD.jpg";
+      #sha256 = "12f275862b66e99973c933b4471574e8d1bb2997b6752c47be510e084c427c9a";
       #url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Cool-Anime-Wallpaper-HD-Free-Download.jpg";
       #sha256 = "5a265d5c428467ce01a091e4cc0c548dd2652d544e4747731dcb09c51a143802";
+      url = "https://4kwallpapers.com/images/wallpapers/anime-girl-2880x1800-10025.jpg";
+      sha256 = "c96761f3c7100f11d9c644a11abc5cbe33e1e1a6bf317b317f856466457198f3";
     };
 
     fonts = {
