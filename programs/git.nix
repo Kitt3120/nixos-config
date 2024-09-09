1c5ad02b63eaa84cce8a2eb476b552f440eba57c
Torben Schweren <torben@schweren.dev>
2024-09-09 18:12:23 +0200

    add: more git aliases

--- programs/git.nix
+++ programs/git.nix
@@ -25,10 +25,13 @@
         as = "!git add . && git status";
         ds = "diff --staged";
         ads = "!git add . && git diff --staged";
+        pf = "push --force-with-lease";
         bvs = "!git branch -v --sort=committerdate";
         bvas = "!git branch -va --sort=committerdate";
         rebaseFromMain = "!git stash && git checkout main && git fetch --all -p && git pull && git checkout - && git rebase main && git stash pop";
         rebaseFromMaster = "!git stash && git checkout master && git fetch --all -p && git pull && git checkout - && git rebase master && git stash pop";
+        mergeFromMain = "!git stash && git checkout main && git fetch --all -p && git pull && git checkout - && git merge main && git stash pop";
+        mergeFromMaster = "!git stash && git checkout master && git fetch --all -p && git pull && git checkout - && git merge master && git stash pop";
       };
     };
   };
