d226926e7ef87995a2b97fb32f8b80491abc0513
Torben Schweren <torben@schweren.dev>
2024-10-04 19:08:38 +0200

    add: more git configuration

--- programs/git.nix
+++ programs/git.nix
@@ -14,12 +14,17 @@
       };
 
       init.defaultBranch = "main";
+      checkout.defaultRemote = "origin";
+      push.autoSetupRemote = "true";
+
       gpg.format = "ssh";
       commit.gpgsign = true;
       tag.gpgsign = true;
+
       rerere.enabled = true;
-      column.ui = "auto";
+
       fetch.writeCommitGraph = true;
+      column.ui = "auto";
 
       alias = {
         as = "!git add . && git status";
@@ -29,10 +34,12 @@
         pf = "push --force-with-lease";
         bvs = "!git branch -v --sort=committerdate";
         bvas = "!git branch -va --sort=committerdate";
-        rebaseFromMain = "!git stash && git checkout main && git fetch --all -p && git pull && git checkout - && git rebase main && git stash pop";
-        rebaseFromMaster = "!git stash && git checkout master && git fetch --all -p && git pull && git checkout - && git rebase master && git stash pop";
+        rebaseToMain = "!git stash && git checkout main && git fetch --all -p && git pull && git checkout - && git rebase main && git stash pop";
+        rebaseToMaster = "!git stash && git checkout master && git fetch --all -p && git pull && git checkout - && git rebase master && git stash pop";
+        rebaseToStaging = "!git stash && git checkout staging && git fetch --all -p && git pull && git checkout - && git rebase staging && git stash pop";
         mergeFromMain = "!git stash && git checkout main && git fetch --all -p && git pull && git checkout - && git merge main && git stash pop";
         mergeFromMaster = "!git stash && git checkout master && git fetch --all -p && git pull && git checkout - && git merge master && git stash pop";
+        mergeFromStaging = "!git stash && git checkout staging && git fetch --all -p && git pull && git checkout - && git merge staging && git stash pop";
       };
     };
   };
