cf46ea7ce0a92554b5bbecf959a6bb1adb398a4a
Torben Schweren <torben@schweren.dev>
2024-11-16 14:02:56 +0100

    add: libvirt tpm support

--- virtualization/libvirt.nix
+++ virtualization/libvirt.nix
@@ -1,16 +1,32 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.swtpm.enable = true;
+  };
+
   programs.virt-manager.enable = true;
-  environment.systemPackages = with pkgs; [ virtiofsd ];
+
+  environment.systemPackages = with pkgs; [
+    virtiofsd
+    libguestfs
+    guestfs-tools
+  ];
 
   home-manager.users = config.mapAllUsersToSet (user: {
-    "${user}".dconf.settings = {
-      "org/virt-manager/virt-manager/connections" = {
-        autoconnect = [ "qemu:///system" ];
-        uris = [ "qemu:///system" ];
+    "${user}" = {
+      dconf.settings = {
+        "org/virt-manager/virt-manager/connections" = {
+          autoconnect = [ "qemu:///system" ];
+          uris = [ "qemu:///system" ];
+        };
       };
+
+      xdg.configFile."libvirt/qemu.conf".text = ''
+        # Adapted from /var/lib/libvirt/qemu.conf
+        # Note that AAVMF and OVMF are for Aarch64 and x86 respectively
+        nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ]'';
     };
   });
 }
