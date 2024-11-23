c6940882365b89bae99524e61d15fb8a0550f177
Torben Schweren <torben@schweren.dev>
2024-11-23 21:15:52 +0100

    fix: openrgb kernel modules

--- services/openrgb.nix
+++ services/openrgb.nix
@@ -1,6 +1,16 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   environment.systemPackages = with pkgs; [ pkgs.openrgb-with-all-plugins ];
   services.udev.packages = [ pkgs.openrgb-with-all-plugins ];
+
+  boot.kernelModules =
+    [ "i2c-dev" ]
+    ++ lib.optionals (config.hardware.cpu.intel.updateMicrocode == "amd") [ "i2c-piix4" ]
+    ++ lib.optionals (config.hardware.cpu.amd.updateMicrocode == "intel") [ "i2c-i801" ];
 }
