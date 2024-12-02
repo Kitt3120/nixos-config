200efa5c3af5259c7ce383032591353727cd7f73
Torben Schweren <torben@schweren.dev>
2024-12-02 20:42:34 +0100

    revert: switch back to nixpkgs-mozilla master

--- programs/firefox-nightly.nix
+++ programs/firefox-nightly.nix
@@ -5,8 +5,7 @@
     let
       moz-rev = "master";
       moz-tarball = builtins.fetchTarball {
-        #url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz";
-        url = "https://github.com/hneiva/nixpkgs-mozilla/archive/refs/heads/hneiva/xz-builds.zip"; # TODO: Change back when PR is merged:https://github.com/mozilla/nixpkgs-mozilla/pull/333
+        url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz";
       };
       nightlyOverlay = (import "${moz-tarball}/firefox-overlay.nix");
     in
