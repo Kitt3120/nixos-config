f249694ea004903793e69d9099ada364b672952d
Torben Schweren <torben@schweren.dev>
2024-12-29 23:47:11 +0100

    disable: broken anime-game launchers

--- programs/an-anime-team-launchers.nix
+++ programs/an-anime-team-launchers.nix
@@ -10,10 +10,10 @@ in
     aagl.module
   ];
 
-  programs.anime-game-launcher.enable = true;
+  #programs.anime-game-launcher.enable = true;
   programs.anime-games-launcher.enable = true;
   programs.honkers-railway-launcher.enable = true;
   programs.honkers-launcher.enable = true;
-  programs.wavey-launcher.enable = true;
-  programs.sleepy-launcher.enable = true;
+  #programs.wavey-launcher.enable = true;
+  #programs.sleepy-launcher.enable = true;
 }
