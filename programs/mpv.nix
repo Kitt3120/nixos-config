d0d0d0d759427048cf7d25a1c26aa7f3852aa052
Torben Schweren <torben@schweren.dev>
2025-01-18 21:37:04 +0100

    add: mpv settings

--- programs/mpv.nix
+++ programs/mpv.nix
@@ -2,4 +2,12 @@
 
 {
   environment.systemPackages = with pkgs; [ mpv-unwrapped ];
+
+  environment.etc."mpv/mpv.conf".text = ''
+    sub-auto=fuzzy
+    profile=high-quality
+    video-sync=display-resample
+    interpolation
+    hwdec=auto
+  '';
 }
