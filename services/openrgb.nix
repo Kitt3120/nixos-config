d2a581b51ecd952136b28d3626699c5d6010387d
Torben Schweren <torben@schweren.dev>
2025-01-18 22:36:48 +0100

    update: openrgb hash

--- services/openrgb.nix
+++ services/openrgb.nix
@@ -15,7 +15,7 @@ let
       owner = "CalcProgrammer1";
       repo = "OpenRGB";
       rev = "master";
-      sha256 = "sha256-uZxFr5PHprsTVOIsO9o39hon6DH5LzEIAYiUTQeziTg=";
+      sha256 = "sha256-LiI/RWFvW4sxeGNn3PT+jiTiO0BUt72mHu/UFGWBqGU=";
     };
 
     postPatch = ''
