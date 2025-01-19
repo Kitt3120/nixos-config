dd45e6f33bfeb90a38571b63a280e817892ab05e
Torben Schweren <torben@schweren.dev>
2025-01-19 03:10:55 +0100

    add: pipewire raopOpenFirewall

--- desktop/pipewire.nix
+++ desktop/pipewire.nix
@@ -5,12 +5,13 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    wireplumber.enable = true;
     socketActivation = true;
     audio.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-    wireplumber.enable = true;
+    raopOpenFirewall = true;
   };
 }
