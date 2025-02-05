1f71fb13c4e09755cd8db79746f746d3b15a6147
Torben Schweren <torben@schweren.dev>
2025-02-05 19:19:39 +0100

    fix: osu! (Wayland) desktop entry

--- programs/osu-lazer.nix
+++ programs/osu-lazer.nix
@@ -10,15 +10,18 @@
           # Desktop Entry Specification: https://standards.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
 
           [Desktop Entry]
+          Version=1.5
           Type=Application
           Name=osu! (Wayland)
           Comment=A free-to-win rhythm game. Rhythm is just a *click* away!
-          Icon=osu!
-          Exec=env COMPlus_GCGen0MaxBudget=600000 SDL_VIDEODRIVER=wayland osu!
+          Icon=osu
+          Exec=env COMPlus_GCGen0MaxBudget=600000 SDL_VIDEODRIVER=wayland osu! %u
           Terminal=false
+          MimeType=application/x-osu-beatmap-archive;application/x-osu-skin-archive;application/x-osu-beatmap;application/x-osu-storyboard;application/x-osu-replay;x-scheme-handler/osu;
           Categories=Game;
           StartupWMClass=osu!
           SingleMainWindow=true
+          StartupNotify=true
         '';
       })
     else
