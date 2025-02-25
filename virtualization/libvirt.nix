1d5f838f38c37528bd2c384eb4d3304966feb529
Torben Schweren <torben@schweren.dev>
2025-02-25 18:58:39 +0100

    fix: libvirt

--- virtualization/libvirt.nix
+++ virtualization/libvirt.nix
@@ -3,7 +3,20 @@
 {
   virtualisation.libvirtd = {
     enable = true;
-    qemu.swtpm.enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMFFull.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
   };
 
   programs.virt-manager.enable = true;
@@ -16,17 +29,11 @@
 
   home-manager.users = config.mapAllUsersToSet (user: {
     "${user}" = {
-      dconf.settings = {
-        "org/virt-manager/virt-manager/connections" = {
-          autoconnect = [ "qemu:///system" ];
-          uris = [ "qemu:///system" ];
-        };
-      };
-
       xdg.configFile."libvirt/qemu.conf".text = ''
         # Adapted from /var/lib/libvirt/qemu.conf
         # Note that AAVMF and OVMF are for Aarch64 and x86 respectively
-        nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ]'';
+        nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ]
+      '';
     };
   });
 }
