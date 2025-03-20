79392ea80cf97146c74d69d67a2bcd08255f095f
Torben Schweren <torben@schweren.dev>
2025-03-20 12:09:39 +0100

    fix: zoxide shell hook for bash & zsh

--- programs/zoxide.nix
+++ programs/zoxide.nix
@@ -8,7 +8,7 @@
 {
   environment.systemPackages = with pkgs; [ zoxide ];
 
-  programs.bash.interactiveShellInit = "eval $(zoxide init --cmd bash)";
-  programs.zsh.interactiveShellInit = lib.mkIf config.programs.zsh.enable "eval $(zoxide init --cmd zsh)";
+  programs.bash.interactiveShellInit = "eval $(zoxide init --cmd cd bash)";
+  programs.zsh.interactiveShellInit = lib.mkIf config.programs.zsh.enable "eval $(zoxide init --cmd cd zsh)";
   programs.fish.interactiveShellInit = lib.mkIf config.programs.fish.enable "zoxide init --cmd cd fish | source";
 }
