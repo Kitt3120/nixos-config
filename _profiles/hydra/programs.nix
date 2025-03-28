4b5045e5f89f43efbc13a52503d02555d1ba6fc3
Torben Schweren <torben@schweren.dev>
2025-03-28 21:33:52 +0100

    fix: make configs more consistent

--- _profiles/hydra/programs.nix
+++ _profiles/hydra/programs.nix
@@ -32,7 +32,7 @@
     ../../programs/inkscape.nix
     ../../programs/jellyfin.nix
     ../../programs/jetbrains-toolbox.nix
-    #../../programs/john.nix #TODO: Enable again
+    #../../programs/john.nix #TODO: Enable again when fixed
     ../../programs/kdenlive.nix
     ../../programs/kitty.nix
     ../../programs/krita.nix
@@ -44,7 +44,7 @@
     ../../programs/lshw-gui.nix
     ../../programs/mangohud.nix
     ../../programs/maven.nix
-    #../../programs/mdk4.nix #TODO: Enable again
+    #../../programs/mdk4.nix #TODO: Enable again when fixed
     ../../programs/metasploit.nix
     ../../programs/mkvtoolnix.nix
     ../../programs/mpv.nix
