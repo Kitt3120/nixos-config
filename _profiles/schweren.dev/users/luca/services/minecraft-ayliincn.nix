5fb8af4f0aa90198d34025451f1bb4778ad89321
Torben Schweren <torben@schweren.dev>
2025-04-14 01:19:35 +0200

    fix: minecraft-ayliincn

--- _profiles/schweren.dev/users/luca/services/minecraft-ayliincn.nix
+++ _profiles/schweren.dev/users/luca/services/minecraft-ayliincn.nix
@@ -2,13 +2,17 @@
 
 let
   ayliincn =
-    config.mkMinecraftServer "ayliincn" "/home/luca/ayliincn" "paper" pkgs.jdk21 "15G"
+    config.mkMinecraftServer "ayliincn" "/home/luca/ayliincn"
+      "https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/224/downloads/paper-1.21.4-224.jar"
+      "sha256-DN5RmWlReUorNF95au0FBuWuDeslqwhDGWf2NkTihxc="
+      pkgs.jdk21
+      "15G"
       false; # TODO: Put Minecraft servers into settings
 in
 {
   home-manager.users.luca = {
     home.packages = [
-      ayliincn.scripts.manager
+      ayliincn.scripts.cli
       ayliincn.scripts.start
       ayliincn.scripts.stop
     ];
