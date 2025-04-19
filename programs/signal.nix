6a8e7034c241c1894d5f7d0518676f009b779c5f
Torben Schweren <torben@schweren.dev>
2025-04-19 02:47:06 +0200

    fix: adapt to upstream nixpkgs changes

--- programs/signal.nix
+++ programs/signal.nix
@@ -2,5 +2,5 @@
 
 {
   # If signal-desktop starts minimized, edit ~/.config/Signal/ephemeral.json and set "maximized" to true
-  environment.systemPackages = with pkgs; [ signal-desktop-source ];
+  environment.systemPackages = with pkgs; [ signal-desktop ];
 }
