7916264edababa3ed98f5c5d9fa5c9cb4278bc54
Torben Schweren <torben@schweren.dev>
2025-05-18 15:24:02 +0200

    refactor: gitconfig

--- programs/git.nix
+++ programs/git.nix
@@ -5,38 +5,82 @@
     enable = true;
     lfs.enable = true;
     prompt.enable = true;
+
     config = {
       core = {
         autocrlf = "input";
-        editor = "nano";
+        editor = "${pkgs.vim}/bin/vim";
         untrackedCache = true;
+        fsmode = true;
         fsmonitor = true;
+        preloadIndex = true;
+        pager = "${pkgs.delta}/bin/delta";
       };
 
       init.defaultBranch = "main";
-      checkout.defaultRemote = "origin";
       push.autoSetupRemote = "true";
+
+      checkout = {
+        defaultRemote = "origin";
+        workers = 0;
+      };
 
       gpg.format = "ssh";
       commit.gpgsign = true;
       tag.gpgsign = true;
 
+      fetch = {
+        writeCommitGraph = true;
+        all = true;
+        prune = true;
+        parallel = 0;
+      };
+      submodule.fetchJobs = 0;
+
+      diff.algorithm = "histogram";
+      column.ui = "auto";
       rerere.enabled = true;
 
-      fetch.writeCommitGraph = true;
-      column.ui = "auto";
-
       alias = {
-        as = "!git add . && git status";
+        st = "status";
+        d = "diff";
+        dmy = "diff --diff-algorithm=myers";
+        dmi = "diff --diff-algorithm=minimal";
+        dp = "diff --diff-algorithm=patience";
+        dh = "diff --diff-algorithm=histogram";
         ds = "diff --staged";
-        ads = "!git add . && git diff --staged";
-        fp = "!git fetch --all -p && git pull";
-        pf = "push --force-with-lease";
+        dsmy = "diff --staged --diff-algorithm=myers";
+        dsmi = "diff --staged --diff-algorithm=minimal";
+        dsp = "diff --staged --diff-algorithm=patience";
+        dsh = "diff --staged --diff-algorithm=histogram";
+
+        ch = "checkout";
+        br = "branch";
         bvs = "!git branch -v --sort=committerdate";
         bvas = "!git branch -va --sort=committerdate";
+
+        fap = "!git fetch --all -p && git pull";
+        fep = "!git fetch --all -p && git pull";
+        puf = "push --force-with-lease";
+
+        as = "!git add . && git status";
+        ads = "!git add . && git diff --staged";
+
+        co = "commit";
+        ca = "commit --amend";
+        cm = "commit -m";
+        cowip = "commit -m \"WIP\"";
+
+        rba = "rebase --abort";
+        rbc = "rebase --continue";
+
+        ma = "merge --abort";
+        mc = "merge --continue";
+
         rebaseToMain = "!git stash && git checkout main && git fetch --all -p && git pull && git checkout - && git rebase main && git stash pop";
         rebaseToMaster = "!git stash && git checkout master && git fetch --all -p && git pull && git checkout - && git rebase master && git stash pop";
         rebaseToStaging = "!git stash && git checkout staging && git fetch --all -p && git pull && git checkout - && git rebase staging && git stash pop";
+
         mergeFromMain = "!git stash && git checkout main && git fetch --all -p && git pull && git checkout - && git merge main && git stash pop";
         mergeFromMaster = "!git stash && git checkout master && git fetch --all -p && git pull && git checkout - && git merge master && git stash pop";
         mergeFromStaging = "!git stash && git checkout staging && git fetch --all -p && git pull && git checkout - && git merge staging && git stash pop";
