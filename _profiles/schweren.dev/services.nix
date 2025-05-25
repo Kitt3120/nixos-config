91aa2a7bbc724c0d0cd5788d51c0100359010222
Torben Schweren <torben@schweren.dev>
2025-05-26 01:03:48 +0200

    remove: deprecated minecraft servers

--- _profiles/schweren.dev/services.nix
+++ _profiles/schweren.dev/services.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ../../services/minecraft-servers.nix
     ../../services/tor-relay.nix
     ../../services/vscode-server.nix
     ../../services/zabbix-agent.nix
--- _profiles/schweren.dev/users/minecraft.nix
+++ _profiles/schweren.dev/users/minecraft.nix
@@ -4,11 +4,5 @@
   imports = [
     ./minecraft/ssh.nix
     ./minecraft/user.nix
-    #./minecraft/services/minecraft-mdj.nix
-    #./minecraft/services/minecraft-scary.nix
-    #./minecraft/services/minecraft-adventure.nix
-    #./minecraft/services/minecraft-dud.nix
-    #./minecraft/services/minecraft-redstone.nix
-    #./minecraft/services/minecraft-horror.nix
   ];
 }
