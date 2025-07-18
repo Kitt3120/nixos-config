76a9d5910d50489272deeecda6f1befc5986c9ec
Torben Schweren <torben@schweren.dev>
2025-07-18 14:10:17 +0200

    remove: other an-anime-team launchers besides anime-games-launcher

--- programs/an-anime-team-launchers.nix
+++ programs/an-anime-team-launchers.nix
@@ -10,10 +10,11 @@ in
     aagl.module
   ];
 
-  #programs.anime-game-launcher.enable = true;
   programs.anime-games-launcher.enable = true;
-  programs.honkers-railway-launcher.enable = true;
-  programs.honkers-launcher.enable = true;
-  #programs.wavey-launcher.enable = true;
-  #programs.sleepy-launcher.enable = true;
+
+  programs.anime-game-launcher.enable = false;
+  programs.honkers-railway-launcher.enable = false;
+  programs.honkers-launcher.enable = false;
+  programs.wavey-launcher.enable = false;
+  programs.sleepy-launcher.enable = false;
 }
