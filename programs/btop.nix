f0206df54875e71b17b7b2df5f630f619137b34c
Torben Schweren <torben@schweren.dev>
2025-07-23 03:54:11 +0200

    add: AMD and NVIDIA support for btop

--- programs/btop.nix
+++ programs/btop.nix
@@ -1,5 +1,19 @@
-{ config, pkgs, ... }:
-
 {
-  environment.systemPackages = with pkgs; [ btop ];
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  btop =
+    if builtins.elem "amdgpu" config.boot.initrd.kernelModules then
+      pkgs.btop-rocm
+    else if config.hardware.nvidia.enabled then
+      pkgs.btop-nvidia
+    else
+      pkgs.btop;
+in
+{
+  environment.systemPackages = [ btop ];
 }
