35ea3243b21851f72cf2b479b3366fc36334da58
Torben Schweren <torben@schweren.dev>
2025-08-13 19:51:05 +0200

    refactor: use modesetting driver instead of amdgpu for xserver

--- desktop/xserver.nix
+++ desktop/xserver.nix
@@ -1,16 +1,10 @@
 { config, pkgs, ... }:
 
-let
-  isAmdGpu =
-    config.hardware.amdgpu.initrd.enable
-    || config.hardware.amdgpu.opencl.enable
-    || config.hardware.amdgpu.overdrive.enable;
-in
 {
   services.xserver = {
     enable = true;
     enableTCP = false;
-    videoDrivers = if isAmdGpu then [ "amdgpu" ] else [ ];
+    videoDrivers = [ "modesetting" ];
     xkb = {
       layout = "de";
       variant = "deadgraveacute";
