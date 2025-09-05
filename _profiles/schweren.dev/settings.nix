1ec06d93a35d9e8b35fc250d70322c2a1184fad9
Torben Schweren <torben@schweren.dev>
2025-09-05 23:03:59 +0200

    refactor: switch minecraft servers on schweren.dev

--- _profiles/schweren.dev/settings.nix
+++ _profiles/schweren.dev/settings.nix
@@ -40,13 +40,33 @@
     };
 
     minecraft.servers = [
+      # {
+      #   user = "minecraft";
+      #   name = "mit-den-hoes";
+      #   directory = "/home/minecraft/mit-den-hoes";
+      #   jar = {
+      #     url = "https://meta.fabricmc.net/v2/versions/loader/1.21.4/0.16.14/1.0.3/server/jar";
+      #     hash = "sha256-xQdPurp4NNQ9m77QvhWXNeeYv+Tq9zXCXIJeZZ/iqwM=";
+      #   };
+      #   java = pkgs.jdk21;
+      #   ram = "15G";
+      #   debug = false;
+      #   ports = {
+      #     tcp = [
+      #       25565
+      #       8123
+      #     ];
+      #     udp = [ 24454 ];
+      #   };
+      # }
+
       {
         user = "minecraft";
-        name = "mit-den-hoes";
-        directory = "/home/minecraft/mit-den-hoes";
+        name = "dem-bois";
+        directory = "/home/minecraft/dem-bois";
         jar = {
-          url = "https://meta.fabricmc.net/v2/versions/loader/1.21.4/0.16.14/1.0.3/server/jar";
-          hash = "sha256-xQdPurp4NNQ9m77QvhWXNeeYv+Tq9zXCXIJeZZ/iqwM=";
+          url = "https://meta.fabricmc.net/v2/versions/loader/1.21.8/0.17.2/1.1.0/server/jar";
+          hash = "sha256-gPZubbZF6/qx+nDxeCguKkGyAsnzSKC860lnwN0sdSI=";
         };
         java = pkgs.jdk21;
         ram = "15G";
