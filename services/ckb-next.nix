27bd4c74281a36f3c9671d37a67c479854f0f0a0
Torben Schweren <torben@schweren.dev>
2025-09-26 23:11:47 +0200

    fix: ckb-next build failure because of DBUS

--- services/ckb-next.nix
+++ services/ckb-next.nix
@@ -1,5 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  hardware.ckb-next.enable = true;
+  # TODO: Remove workaround
+  # Thanks to IncredibleLaser! https://github.com/NixOS/nixpkgs/issues/444209#issuecomment-3315031496
+  hardware.ckb-next = {
+    enable = true;
+    package = pkgs.ckb-next.overrideAttrs (old: {
+      cmakeFlags = (old.cmakeFlags or [ ]) ++ [ "-DUSE_DBUS_MENU=0" ];
+    });
+  };
 }
