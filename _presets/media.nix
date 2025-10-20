bdf676523064025100f83c756cce58c72d59d97b
Torben Schweren <torben@schweren.dev>
2025-10-20 22:42:38 +0200

    enable: audacity, as it has been fixed upstream

--- _presets/media.nix
+++ _presets/media.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ../programs/ani-cli.nix
-    #../programs/audacity.nix # TODO: Enable again when fixed
+    ../programs/audacity.nix
     ../programs/blender.nix
     ../programs/davinci-resolve-studio.nix
     ../programs/easyeffects.nix
