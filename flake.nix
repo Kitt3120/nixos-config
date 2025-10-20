01d2f5d28f23d5ab5b8907aa0fd81c6506e3ba34
Torben Schweren <torben@schweren.dev>
2025-10-20 21:50:02 +0200

    update: flake inputs

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   # Don't forget to add new inputs to autoUpgrade.nix
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs?ref=master";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
