a1193c7c3c43c9f8895823c070434f98dddc0704
Torben Schweren <torben@schweren.dev>
2025-11-14 22:20:52 +0100

    add: retroarch to modulosaurus' sunshine

--- _profiles/modulosaurus/settings.nix
+++ _profiles/modulosaurus/settings.nix
@@ -53,6 +53,15 @@
           ];
           auto-detach = "true";
         }
+        {
+          name = "Retroarch";
+          image-path = builtins.fetchurl {
+            url = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.steamgriddb.com%2Fgrid%2F8fb49549f3fd57ad0a80e57825f9e41e.png&f=1&nofb=1&ipt=20215f50499b0ae3bcce7872bc85b13ec7584a6fc8963c02df4ceb5b2b1370ff";
+            name = "retroarch.png";
+            sha256 = "sha256:09qkfflbqbxc5ndwbsa7s102pmg7lyqvrc25g1crkp58jn2idsdv";
+          };
+          cmd = "sudo -u torben setsid retroarch";
+        }
       ];
     };
   };
