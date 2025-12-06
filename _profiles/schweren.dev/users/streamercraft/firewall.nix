3e3073168091dbf62000bb9306080bdce070c0d1
Torben Schweren <torben@schweren.dev>
2025-12-06 05:25:55 +0100

    add: geyser firewall rule for streamcraft

--- _profiles/schweren.dev/users/streamercraft/firewall.nix
+++ _profiles/schweren.dev/users/streamercraft/firewall.nix
@@ -2,12 +2,21 @@
 
 {
   networking.firewall = {
+    allowedTCPPorts = [ ];
+
+    allowedUDPPorts = [
+      19132 # Geyser
+    ];
+
+    # MC Servers
     allowedTCPPortRanges = [
       {
         from = 25565;
         to = 25599;
       }
     ];
+
+    # MC Servers
     allowedUDPPortRanges = [
       {
         from = 25565;
