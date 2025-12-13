1b86d7bf3e099c953ca346a7d82393bae03a3b30
Torben Schweren <torben@schweren.dev>
2025-12-13 05:43:57 +0100

    add: temporary opendeck implementation against local nixpkgs

--- services/opendeck.nix
+++ services/opendeck.nix
@@ -1,4 +1,18 @@
 { pkgs, ... }:
 
+let
+  pkgs-local = import /home/torben/Nextcloud/Programmierung/NixOS/nixpkgs {
+    system = pkgs.system;
+    config = pkgs.config;
+  };
+in
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      opendeck = pkgs-local.opendeck;
+    })
+  ];
+
+  environment.systemPackages = with pkgs; [ opendeck ];
+  services.udev.packages = with pkgs; [ opendeck ];
 }
