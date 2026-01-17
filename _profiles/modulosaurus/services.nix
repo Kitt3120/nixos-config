d3a846240e91f58e94c72cb5053fc4ab8777fbd0
Torben Schweren <torben@schweren.dev>
2026-01-17 17:48:36 +0100

    disable: opendeck for now, as I have to update my out-of-tree implementation

--- _profiles/modulosaurus/services.nix
+++ _profiles/modulosaurus/services.nix
@@ -7,7 +7,7 @@
     ../../services/logitech.nix
     ../../services/media-pipelines-ffmpeg.nix
     ../../services/mullvad.nix
-    ../../services/opendeck.nix
+    #../../services/opendeck.nix # TODO: Enable again
     ../../services/openrgb.nix
     ../../services/opentabletdriver.nix
     ../../services/ratbagd.nix
