e459d96856e73f01ae82f8e244e427377f3ca7f9
Torben Schweren <torben@schweren.dev>
2026-01-28 22:47:37 +0100

    add jdk25

--- programs/java.nix
+++ programs/java.nix
@@ -9,6 +9,7 @@
   environment.systemPackages = with pkgs; [
     # First option is default Java
     jdk
+    jdk25
     jdk21
     jdk17
     jdk8
@@ -17,6 +18,7 @@
   environment.variables = {
     JAVA_HOME = "${pkgs.jdk}";
     JAVA_LATEST_HOME = "${pkgs.jdk}";
+    JAVA_25_HOME = "${pkgs.jdk25}";
     JAVA_21_HOME = "${pkgs.jdk21}";
     JAVA_17_HOME = "${pkgs.jdk17}";
     JAVA_8_HOME = "${pkgs.jdk8}";
