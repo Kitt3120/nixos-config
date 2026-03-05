91c87bd65d8bfa20623eb969615a19a03a192ac6
Torben Schweren <torben@schweren.dev>
2026-03-05 23:47:25 +0100

    remove: comment for old workaround of signal desktop problem

--- programs/signal.nix
+++ programs/signal.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 
 {
-  # If signal-desktop starts minimized, edit ~/.config/Signal/ephemeral.json and set "maximized" to true
   environment.systemPackages = with pkgs; [ signal-desktop ];
 }
