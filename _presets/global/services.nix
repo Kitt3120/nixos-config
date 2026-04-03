4c959d7bd58f5f8e6ffc4ac33fae076a173fd354
Torben Schweren <torben@schweren.dev>
2026-04-04 01:31:39 +0200

    disable: cups as it is broken upstream

--- _presets/global/services.nix
+++ _presets/global/services.nix
@@ -4,7 +4,7 @@
   imports = [
     ../../services/avahi.nix
     ../../services/comin.nix
-    ../../services/cups.nix
+    #../../services/cups.nix # TODO: Enable again when fixed upstream
     ../../services/dbus.nix
     ../../services/fstrim.nix
     ../../services/fwupd.nix
