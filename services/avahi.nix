35d8a82fbb144465d7eeff99284c31b71bd6bac7
Torben Schweren <torben@schweren.dev>
2026-05-07 21:15:27 +0200

    fix: cups kyocera

--- services/avahi.nix
+++ services/avahi.nix
@@ -5,5 +5,7 @@
     enable = true;
     publish.enable = true;
     openFirewall = true;
+    nssmdns4 = true;
+    nssmdns6 = true;
   };
 }
--- services/cups.nix
+++ services/cups.nix
@@ -13,7 +13,6 @@
       cups-pk-helper
       cups-filters
       ghostscript
-      cups-kyodialog
       cups-kyocera
       foomatic-db
       foomatic-db-engine
@@ -31,25 +30,6 @@
 
   services.system-config-printer.enable = true;
   programs.system-config-printer.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    cups-pk-helper
-    cups-filters
-    cups-kyodialog
-    cups-kyocera
-    ghostscript
-    foomatic-db
-    foomatic-db-engine
-    foomatic-db-ppds
-    foomatic-db-nonfree
-    foomatic-db-ppds-withNonfreeDb
-    foomatic-filters
-    gutenprint
-    gutenprintBin
-    hplip
-    hplipWithPlugin
-    splix
-  ];
 
   # TODO: remove
   nixpkgs.config.permittedInsecurePackages = [
