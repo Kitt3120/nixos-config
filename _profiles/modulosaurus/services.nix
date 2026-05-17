c3f33698c748debf1cdedd5b7ada4db905cf8673
Torben Schweren <torben@schweren.dev>
2026-05-18 01:05:36 +0200

    enable: local OpenDeck build for modulosaurus

--- _profiles/modulosaurus/services.nix
+++ _profiles/modulosaurus/services.nix
@@ -6,7 +6,7 @@
     ../../services/logitech.nix
     ../../services/media-pipelines-ffmpeg.nix
     ../../services/mullvad.nix
-    #../../services/opendeck.nix # TODO: Enable again
+    ../../services/opendeck.nix
     ../../services/openrgb.nix
     ../../services/opentabletdriver.nix
     ../../services/ratbagd.nix
