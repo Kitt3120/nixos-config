fb08778aed3d7c552b58346d8cb6b738525d10a3
Torben Schweren <torben@schweren.dev>
2026-05-24 21:45:43 +0200

    fix: enable lutris and add fix for openldap failure

--- programs/lutris.nix
+++ programs/lutris.nix
@@ -1,5 +1,15 @@
 { pkgs, ... }:
 
 {
+  # TODO: Remove workaround
+  # See https://github.com/NixOS/nixpkgs/issues/513245#issuecomment-4320293674
+  nixpkgs.overlays = [
+    (_: prev: {
+      openldap = prev.openldap.overrideAttrs {
+        doCheck = !prev.stdenv.hostPlatform.isi686;
+      };
+    })
+  ];
+
   environment.systemPackages = with pkgs; [ lutris ];
 }
