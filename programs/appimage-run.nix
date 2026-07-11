7e0e67dd8ffb3f802e48b85dda3783ce55a197f9
Torben Schweren <torben@schweren.dev>
2026-07-11 17:39:24 +0200

    fix: missing libraries in appimage-run

--- programs/appimage-run.nix
+++ programs/appimage-run.nix
@@ -4,5 +4,13 @@
   programs.appimage = {
     enable = true;
     binfmt = true;
+
+    # extra packages to include in the AppImage runtime environment needed by some AppImages
+    package = pkgs.appimage-run.override {
+      extraPkgs =
+        pkgs: with pkgs; [
+          icu # needed by dotnet apps
+        ];
+    };
   };
 }
