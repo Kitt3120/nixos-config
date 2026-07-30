3e4f63a6a6628d02364125cfda294096a85f9dc2
Torben Schweren <torben@schweren.dev>
2026-07-30 20:42:27 +0200

    add: minecraft-eternity Server Icon

--- _profiles/schweren.dev/deployments/minecraft-eternity.nix
+++ _profiles/schweren.dev/deployments/minecraft-eternity.nix
@@ -145,6 +145,8 @@
               USE_MEOWICE_FLAGS: "true"
               SERVER_NAME: "Eternity"
               MOTD: "§6The server running for §5eternity§6. Started §529.07.2026§6."
+              ICON: "https://cloud.schweren.dev/apps/files_sharing/publicpreview/kMHzyqTswdN9GER?file=/&fileId=8367804&x=3840&y=2160&a=true&etag=fa316cd23f8713936c4783760afadbcb"
+              OVERRIDE_ICON: "true"
             volumes:
               - ./eternity:/data
             logging:
