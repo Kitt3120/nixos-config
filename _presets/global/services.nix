c7377e9a1759dccefc8ffa8b40b102861700d465
Torben Schweren <torben@schweren.dev>
2026-08-09 23:29:48 +0200

    add: power-profiles-daemon globally

--- _presets/global/services.nix
+++ _presets/global/services.nix
@@ -14,6 +14,7 @@
     ../../services/openssh.nix
     ../../services/packagekit.nix
     ../../services/podman-deployments.nix
+    ../../services/power-profiles-daemon.nix
     ../../services/timesyncd.nix
   ];
 }
